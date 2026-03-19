1729f1b0a790be82dcee9cb6da8b2f8f544b8e46
lesiiskoletec <lesiiskoletec@gmail.com>
2026-03-19 16:56:08 +0530

    fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -14,7 +14,9 @@ const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+
 router.post("/signout", signOut);
+router.post("/logout", signOut);
 
 router.post("/student/details", submitStudentDetails);
 
